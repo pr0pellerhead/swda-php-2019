04104826e209fc07d7b38e0577ad6459b48c23d4
pr0pellerhead <pr0pellerhead@users.noreply.github.com>
2019-03-01 12:39:17 +0100

    прв час

---
+++ c1.php
@@ -0,0 +1,123 @@
+<?php 
+// echo "Zdravo svetu!";
+// $ime = "Pero";
+// $Godini = 23;
+// $_polnoleten = true;
+// $pi = 3.14;
+
+// echo $ime;
+// echo "<br/>";
+// echo $Godini;
+// echo $_polnoleten;
+// echo $pi;
+// echo "<h1>".$ime."</h1>";
+?>
+
+<!-- <h2>PEROOOOOOOOO!</h2> -->
+
+<?php
+
+// $br = 7;
+
+// if($br % 2 == 0){
+//     echo "par";
+// } else {
+//     echo "nepar";
+// }
+
+// switch($ime) {
+//     case 'Pero':
+//         echo "Zdravo Pero";
+//         break;
+//     case 'Janko':
+//         echo 'Zdravo Janko';
+//         break;
+//     case "Stanko":
+//         echo 'Stanko Janko';
+//         break;
+//     default:
+//         echo 'Nema zdravo';
+// }
+
+// function sobiranje($a, $b){
+//     echo $a + $b;
+// }
+
+// sobiranje(5, 6);
+
+// function odzemanje($a, $b){
+//     return $a - $b;
+// }
+
+// $res = odzemanje(6, 19);
+// echo $res;
+
+// echo odzemanje(3, 1);
+
+// ****************************************
+
+// function ubavMesec($mesec) {
+//     switch(strlen($mesec) % 2){
+//         case 0:
+//             echo "Ubav mesec";
+//             break;
+//         case 1:
+//             echo "Neubav mesec";
+//             break;
+//     }
+// }
+
+// echo "<br/>";
+// ubavMesec("Januari");
+// echo "<br/>";
+// ubavMesec("Dekemvri");
+// ****************************************************
+?>
+
+
+<?php
+
+echo "lhcbds c87q3r9p87[yq9 d4piuc axcpo ]d0i234-650&#$@*^#%(*&!@^)(#*_!@"; // string
+echo 'lhcbds c87q3r9p87[yq9 d4piuc axcpo ]d0i234-650&#$@*^#%(*&!@^)(#*_!@'; // string
+echo 123; // integer - цел број
+echo 3.14; // float - децимални броеви
+echo true; // boolean (true/false)
+
+$ovoshje = "praska";
+$godini = 53;
+$den = true;
+$visina = 1.73;
+
+echo "Pero e visok $visina metri";
+echo 'Pero e visok '.$visina.' metri';
+
+// + - собирање
+// - - одземање
+// / - делење
+// * - множење
+// % - модул (остаток од целобројно делење)
+// за групирање на математички операции се користат загради ()
+
+$a = 10 * 2 - 3;
+$b = 10 * (2 - 3);
+echo '<br/>'.$a;
+echo '<br/>'.$b;
+
+echo '<br/>'.(5 % 2);
+echo '<br/>'.(9 % 5);
+echo '<br/>'.(11 % 3);
+
+// a == b - проста споредба на левата и десната стана (дали а е еднакво со b)
+echo 4 == 4;
+// a === b - споредба по вредност и тип (дали а има иста вредност и тип во b)
+echo '2' === 2;
+// a < b - дали вредноста на a е помала од вредноста на b
+echo 2 < 1;
+
+?>
+
+
+
+
+
+
